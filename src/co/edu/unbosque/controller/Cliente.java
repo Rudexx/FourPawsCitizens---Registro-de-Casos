09b95028446b4f0bd30c9c5506190c23412b0f3b
unknown <danielalvarez202@gmail.com>
2021-04-02 23:43:59 -0500

    Pequeños avances

--- src/co/edu/unbosque/controller/Cliente.java
+++ src/co/edu/unbosque/controller/Cliente.java
@@ -1,4 +1,29 @@
 package co.edu.unbosque.controller;
 
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Cliente {
+
+    public static void main(String[] args) throws Exception {
+
+        try (var socket = new Socket("127.0.0.1", 59897)) {
+
+            System.out.println("Conectado a: " + socket);
+            System.out.println("Ingrese el mensaje a ser tratado:");
+
+            var scanner = new Scanner(System.in);
+            var in = new Scanner(socket.getInputStream());
+            var out = new PrintWriter(socket.getOutputStream(), true);
+
+            while (scanner.hasNextLine()) {
+                out.println(scanner.nextLine());
+                System.out.println("El mensaje transformado es: " + in.nextLine());
+            }
+
+        }
+
+    }
+
 }
--- src/co/edu/unbosque/controller/Servidor.java
+++ src/co/edu/unbosque/controller/Servidor.java
@@ -1,4 +1,24 @@
 package co.edu.unbosque.controller;
 
+import java.net.ServerSocket;
+import java.util.concurrent.Executors;
+
 public class Servidor {
+
+    public static void main(String[] args) throws Exception {
+
+        try (var listener = new ServerSocket(59897)) {
+
+            System.out.println("El server esta corriendo: ");
+
+            var pool = Executors.newFixedThreadPool(20);
+
+            while (true) {
+                pool.execute(new Tratamiento(listener.accept()));
+            }
+
+        }
+
+    }
+
 }
