0555e637a23f89c36700e996d5fe250938e5d045
unknown <danielalvarez202@gmail.com>
2021-04-04 15:04:24 -0500

    Menu

---
+++ src/co/edu/unbosque/controller/Caso.java
@@ -0,0 +1,98 @@
+package co.edu.unbosque.controller;
+
+public class Caso {
+
+    private String Motivo;
+    private String Especie;
+    private String Tamaño;
+    private String Localidad;
+    private String Direccion;
+    private String NombrePersona;
+    private String Telefono;
+    private String Email;
+    private String Comentarios;
+
+    public Caso(String motivo){
+        Motivo = motivo;
+        Especie = "";
+        Tamaño= "";
+        Localidad= "";
+        Direccion="";
+        NombrePersona="";
+        Telefono= "";
+        Email = "";
+        Comentarios= "";
+    }
+
+    public String getMotivo() {
+        return Motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        Motivo = motivo;
+    }
+
+    public String getEspecie() {
+        return Especie;
+    }
+
+    public void setEspecie(String especie) {
+        Especie = especie;
+    }
+
+    public String getTamaño() {
+        return Tamaño;
+    }
+
+    public void setTamaño(String tamaño) {
+        Tamaño = tamaño;
+    }
+
+    public String getLocalidad() {
+        return Localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        Localidad = localidad;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
+
+    public String getNombrePersona() {
+        return NombrePersona;
+    }
+
+    public void setNombrePersona(String nombrePersona) {
+        NombrePersona = nombrePersona;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        Telefono = telefono;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getComentarios() {
+        return Comentarios;
+    }
+
+    public void setComentarios(String comentarios) {
+        Comentarios = comentarios;
+    }
+}
--- src/co/edu/unbosque/controller/Cliente.java
+++ src/co/edu/unbosque/controller/Cliente.java
@@ -2,6 +2,7 @@ package co.edu.unbosque.controller;
 
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Cliente {
@@ -9,22 +10,58 @@ public class Cliente {
     public static void main(String[] args) throws Exception {
 
         try (var socket = new Socket("127.0.0.1", 59897)) {
+            ArrayList<Caso> casos = new ArrayList();
+            try (var socket = new Socket("127.0.0.1", 59897)) {
 
-            System.out.println("Conectado a: " + socket);
-            System.out.println("Bienvenido a Ciudadanos de 4 Patas");
-            System.out.println("Por favor escriba la operacion a realizar: \n" + "1) Crear caso" + "\n2) Hablar con un Agente");
-            var scanner = new Scanner(System.in);
-            var in = new Scanner(socket.getInputStream());
-            var out = new PrintWriter(socket.getOutputStream(), true);
+                System.out.println("Connected: " + socket);
+                System.out.println("Bienvenidos a animales de 4 patas");
+                var scanner = new Scanner(System.in);
+                System.out.println("""
+                        1.-Crear Caso
+                        2.-Hablar con agente
+                        3.-Salir""");
+                var seleccion = scanner.nextLine();
+                var in = new Scanner(socket.getInputStream());
+                var out = new PrintWriter(socket.getOutputStream(), true);
+                if (seleccion.equals("1")) {
+                    System.out.println("""
+                            -------Creacion del caso--------
+                             Ingrese si fue
+                            1.- Robo
+                            2.- perdida
+                            3.- Abandono
+                            4.- Animal Peligroso
+                            5.- Manejo indebido""");
+                    System.out.println("Introduzca La opcion que desea hacer: ");
+                    seleccion = scanner.nextLine();
+                    casos.add(new Caso(seleccion));
+                    System.out.println("Escriba la especie de la mascota");
+                    String item = scanner.nextLine();
+                    casos.get(1).setEspecie(item);
+                    System.out.println("Escriba el tamaño de la mascota");
+                    item = scanner.nextLine();
+                    casos.get(1).setTamaño(item);
+                    System.out.println("Escribe la localidad de la mascota");
+                    System.out.println("Escribe la direccion de la mascota");
+                    System.out.println("Escriba Su Nombre");
+                    System.out.println("Escriba Su Telefono");
+                    System.out.println("Escriba su Email");
+                    System.out.println("Comentarios Generales");
 
+                } else if (seleccion.equals("2")) {
 
-            while (scanner.hasNextLine()) {
-                out.println(scanner.nextLine());
-                System.out.println("El mensaje transformado es: " + in.nextLine());
+                } else {
+
+                }
+
+                while (scanner.hasNextLine()) {
+                    out.println(scanner.nextLine());
+                    System.out.println("Transformed message: " + in.nextLine());
+                }
+
             }
 
         }
 
     }
-
 }
