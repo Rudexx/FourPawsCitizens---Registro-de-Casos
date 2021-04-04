988e005da2ab6c721bd4e984055d54e80bd93fcb
Rudexx <65428804+Rudexx@users.noreply.github.com>
2021-04-03 21:18:28 -0500

    parte del menú

--- src/co/edu/unbosque/controller/Menu.java
+++ src/co/edu/unbosque/controller/Menu.java
@@ -1,45 +1,142 @@
 package co.edu.unbosque.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.Scanner;
 
-public class Menu {
-	public static void main(String[] args) {
-		
+public class Menu implements Runnable {
+	
+	  private Socket socket;
+	  
+	  public Menu(Socket s) {
+		this.socket = s;
+	  }
 	
 
-		Scanner entrada = new Scanner(System.in);
-		int opcion =0;
-		System.out.println("1.- Crear Caso \n"
-				+"2.-Hablar con agente\n"
-				+"3.-Salir");
-		opcion = entrada.nextInt();
+	
+	public static void safePrintln(String s) {
+        synchronized (System.out) {
+            safePrintln(s);
+        }
+    }
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+
 		
-		switch  (opcion){
-		case 1:{
-			System.out.println("-------Creacion del caso--------" +
-						"\n Ingrese si fue\n"
-						+"1.- Robo\n"
-						+"2.- perdida\n"
-						+"3.- Abandono\n");
-			System.out.println("Introduzca La opcion que desea hacer: ");
-			opcion = entrada.nextInt();
-			switch (opcion) {
-			case 1:
-				System.out.println("Elije un caso");
-				System.out.println("Robo");
-				System.out.println("Perdida");
-				System.out.println("Abandono");
-				System.out.println("Animal peligroso");
-				System.out.println("Manejo indevido del propietario");
-				
-			}
-			
-				
-		}
+		try {
+
+            var in = new Scanner(socket.getInputStream());
+            var out = new PrintWriter(socket.getOutputStream(), true);
+            
+            Scanner entrada = new Scanner(System.in);
+    		int opcion =0;
+    		safePrintln("1.- Crear Caso \n"
+    				+"2.-Hablar con agente\n"
+    				+"3.-Salir");
+    		opcion =in.nextInt();
+    		
+    		switch  (opcion){
+    		case 1:
+    			safePrintln("-------Creacion del caso--------" +
+    						"\n Ingrese si fue\n"
+    						+"1.- Robo\n"
+    						+"2.- perdida\n"
+    						+"3.- Abandono\n");
+    			safePrintln("Introduzca La opcion que desea hacer: ");
+    			opcion = entrada.nextInt();
+    			switch (opcion) {
+    			case 1:
+    				safePrintln(" Elije un caso");
+    				safePrintln("1. Robo");
+    				safePrintln("2. Perdida");
+    				safePrintln("3. Abandono");
+    				safePrintln("4. Animal peligroso");
+    				safePrintln("5. Manejo indebido del propietario");
+    				
+    				opcion = entrada.nextInt();
+    				
+    				
+    				
+    				safePrintln("¿Especie del animal?");
+    				String especie = entrada.nextLine();
+    				safePrintln("¿Tamaño del animal?");
+    				String tamaño = entrada.nextLine();
+    				entrada.next();
+    				safePrintln("¿Localidad del animal?");
+    				String localidad = entrada.nextLine();
+    				entrada.next();
+    				safePrintln("¿Dirección del animal?");
+    				String direccion = entrada.nextLine();
+    				entrada.next();
+    				safePrintln("¿Nombre completo de la persona que reporta?");
+    				String nombreP = entrada.nextLine();
+    				entrada.next();
+    				safePrintln("¿Teléfono de la persona que reporta?");
+    				String telefonoP = entrada.nextLine();
+    				entrada.next();
+    				safePrintln("¿Email de la persona que reporta?");
+    				String emailP = entrada.nextLine();
+    				entrada.next();
+    				safePrintln("¿Comentarios generales?");
+    				String comentarios = entrada.nextLine();
+    				entrada.next();
+
+    				String tipocaso = "";
+    				switch (opcion) {
+					case 1:
+						tipocaso = "Robo";
+						break;
+					case 2:
+						tipocaso = "Pérdida";
+						break;
+					case 3:
+						tipocaso = "Abandono";
+						break;
+					case 4:
+						tipocaso = "Animal Peligroso";
+						break;
+					case 5:
+						tipocaso = "Manejo indebido del propietario";
+						break;
+					default:
+						break;
+					}
+    				
+    				PetCase p = new PetCase();
+    				p.setAddress(direccion);
+    				p.setComments(comentarios);
+    				p.setNeighbourhood(localidad);
+    				p.setReporterEmail(emailP);
+    				p.setReporterName(nombreP);
+    				p.setReporterTelephone(telefonoP);
+    				p.setSize(tamaño);
+    				p.setSpecie(especie);
+    				p.setType(tipocaso);
+
+    			}
+    			break;
+    		case 2:
+    			
+    		}
 		
+        } catch (Exception e) {
+            safePrintln("Error:" + socket);
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+            }
+            safePrintln("Closed: " + socket);
+        }
+	
 		
 		
 	}
+		
 	}
 
-}
+
---
+++ src/co/edu/unbosque/controller/PetCase.java
@@ -0,0 +1,128 @@
+package co.edu.unbosque.controller;
+
+public class PetCase {
+	
+	private String type;
+	private String specie;
+	private String size;
+	private String neighbourhood;
+	private String address;
+	private String reporterName;
+	private String reporterTelephone;
+	private String reporterEmail;
+	private String comments;
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+	/**
+	 * @return the specie
+	 */
+	public String getSpecie() {
+		return specie;
+	}
+	/**
+	 * @param specie the specie to set
+	 */
+	public void setSpecie(String specie) {
+		this.specie = specie;
+	}
+	/**
+	 * @return the size
+	 */
+	public String getSize() {
+		return size;
+	}
+	/**
+	 * @param size the size to set
+	 */
+	public void setSize(String size) {
+		this.size = size;
+	}
+	/**
+	 * @return the neighbourhood
+	 */
+	public String getNeighbourhood() {
+		return neighbourhood;
+	}
+	/**
+	 * @param neighbourhood the neighbourhood to set
+	 */
+	public void setNeighbourhood(String neighbourhood) {
+		this.neighbourhood = neighbourhood;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	/**
+	 * @return the reporterName
+	 */
+	public String getReporterName() {
+		return reporterName;
+	}
+	/**
+	 * @param reporterName the reporterName to set
+	 */
+	public void setReporterName(String reporterName) {
+		this.reporterName = reporterName;
+	}
+	/**
+	 * @return the reporterTelephone
+	 */
+	public String getReporterTelephone() {
+		return reporterTelephone;
+	}
+	/**
+	 * @param reporterTelephone the reporterTelephone to set
+	 */
+	public void setReporterTelephone(String reporterTelephone) {
+		this.reporterTelephone = reporterTelephone;
+	}
+	/**
+	 * @return the reporterEmail
+	 */
+	public String getReporterEmail() {
+		return reporterEmail;
+	}
+	/**
+	 * @param reporterEmail the reporterEmail to set
+	 */
+	public void setReporterEmail(String reporterEmail) {
+		this.reporterEmail = reporterEmail;
+	}
+	/**
+	 * @return the comments
+	 */
+	public String getComments() {
+		return comments;
+	}
+	/**
+	 * @param comments the comments to set
+	 */
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+	
+	
+	
+	
+	
+
+}
