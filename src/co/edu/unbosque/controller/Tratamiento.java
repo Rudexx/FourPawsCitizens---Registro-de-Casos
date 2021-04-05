aad77b4e735130ba95341f7c020d2161fd66f74b
unknown <danielalvarez202@gmail.com>
2021-04-04 22:47:19 -0500

    Esto fue un error :c

--- src/co/edu/unbosque/controller/Tratamiento.java
+++ src/co/edu/unbosque/controller/Tratamiento.java
@@ -1,6 +1,7 @@
 package co.edu.unbosque.controller;
 
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
@@ -29,8 +30,23 @@ public class Tratamiento implements Runnable {
                 var newMessage = message.toUpperCase();
                 safePrintln("The message to be returned is: " + newMessage);
                 out.println(newMessage);
-                safePrintln("Hola estoy aca");
+                try {
+                    Socket enviarmensaje = new Socket("127.0.0.2", 59897);
+                    var inmensaje = new Scanner(enviarmensaje.getInputStream());
+                    var outmensaje = new PrintWriter(enviarmensaje.getOutputStream());
+                    outmensaje.println("Este mensaje viene de otro lugar creo");
+                    ObjectOutputStream ELmensaje = new ObjectOutputStream(enviarmensaje.getOutputStream());
+                    ELmensaje.writeObject(in.nextLine());
+                    enviarmensaje.close();
+                }catch (Exception e){
+
+                }
             }
+            /*try(Socket enviarmensaje = new Socket("127.0.0.1", 59897)){
+                ObjectOutputStream ELmensaje = new ObjectOutputStream(enviarmensaje.getOutputStream());
+                ELmensaje.writeObject(in.nextLine());
+                enviarmensaje.close();
+            }*/
 
         } catch (Exception e) {
             safePrintln("Error:" + socket);
